1d6713623acbaebd1af349a9cf04ab686561c09e
Florian Bauer <floriandominicbauer@gmail.com>
2020-12-07 15:18:01 +0100

    starter files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import { Button, ButtonContainer } from './components/Button';
 
 function App() {
   return (
-    <ButtonContainer>
-      <Button primary={true}>Primary Button</Button>
-      <Button primary={false}>Secondary Button</Button>
-    </ButtonContainer>
+    <></>
   );
 }
 
